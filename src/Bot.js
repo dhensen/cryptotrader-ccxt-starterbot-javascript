c6da89bad9611a80db6023e5708d089cc6f1ac76
Dino Hensen <dino@riddles.io>
2018-05-18 15:36:09 +0200

    - dont copy talib.node from dist, but from src folder - I included a built talib.node in this repo because: talib.node is built   against a specific version of node. On my workstation I run node   v8.6.0, on riddles.io runs 8.7.0. Those versions   are compatible and this talib.node works on riddles.io. - use a native-ext-loader's rewritePath functionallity to rewrite the   talib path to /executable_dir where riddles.io will unpack all sources.

--- src/Bot.js
+++ src/Bot.js
@@ -7,6 +7,12 @@
  */
 'use strict';
 
+const talib = require('talib');
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
 module.exports = class Bot {
     constructor(exchange) {
         this.exchange = exchange;
@@ -22,13 +28,60 @@ module.exports = class Bot {
             initial_stack: null,
             transaction_fee_percent: 0.0,
         };
+
+        console.error('Using talib version: ' + talib.version);
+        // for (var i in talib.functions) {
+        //     console.error(talib.functions[i].name);
+        // }
     }
 
     async step(timebank) {
+        // await this.exchange.fetchOHLCV('ETH/BTC', '30m').then(data => {
+        //     console.error(data);
+        // });
+
         // loading markets is not necessary
-        await this.exchange.createOrder('BTC/USDT', 'market', 'buy', 0.03);
-        return this.exchange.createOrder('BTC/USDT', 'market', 'buy', 0.06);
+        let closeData = [
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            1.1,
+            2,
+            1.1,
+            1.1,
+            1.1,
+        ];
+
+        return this.talibExecute({
+            name: 'SMA',
+            startIdx: 0,
+            endIdx: closeData.length - 1,
+            inReal: closeData,
+            optInTimePeriod: 5,
+        }).then(console.error);
+
+        // return this.exchange.createOrder('BTC/USDT', 'market', 'buy', 0.06);
         // be sure to either await or return the createOrder, else the order will not be flushed in time
+    }
+
+    talibExecute(params) {
+        return new Promise((resolve, reject) => {
+            talib.execute(params, (err, response) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
     }
 
     async stepErrorHandler(err) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -6,5 +7,23 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+    },
+    // externals: ['talib'],
+    module: {
+        rules: [
+            {
+                test: /\.node$/,
+                loader: 'native-ext-loader',
+                options: {
+                    rewritePath: '/executable_dir',
+                },
+            },
+        ],
+    },
+    resolve: {
+        modules: ['src', 'node_modules'],
+        alias: {
+            talib: path.resolve(__dirname, 'src/talib.node'),
+        },
     },
 };
